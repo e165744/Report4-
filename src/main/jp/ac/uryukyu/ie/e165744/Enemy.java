2e969475d95efbb237d6d1072151c6b1e2122a33
e165744 <e165744@ie.u-ryukyu.ac.jp>
2016-11-27 15:07:06 +0900

    ステップ1

--- src/main/jp/ac/uryukyu/ie/e165744/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165744/Enemy.java
@@ -42,7 +42,9 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+        if(hitPoint>=0){
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+        }
         hero.wounded(damage);
     }
 
